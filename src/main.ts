349686659154d7dbfdca0a3ab5d472fbe28bab02
Patrick Klostermann <dev@netflair.at>
2018-05-25 12:52:31 +0200

    added interfaces for classes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
+//place interface
+interface iPlace {
+	image: string;
+	address: string;
+	zip: number;
+	city: string;
+}
 //SUPER place *cant use location = reserved in JS
-class place {
+class place implements iPlace{
 
 	constructor(public image, public address, public zip, public city ){}
 
@@ -8,8 +15,14 @@ class place {
 	}
 }
 
+//restaurant interface
+interface iRestaurant {
+	tel: number;
+	web: string;
+	type: string;
+}
 //place - restaurant 
-class restaurant extends place {
+class restaurant extends place implements iRestaurant {
 
 	constructor(image, address, zip, city, public tel, public web, public type){
 		super(image, address, zip, city);
@@ -17,6 +30,12 @@ class restaurant extends place {
 
 }
 
+//concert interface
+interface iConcert {
+	date: string;
+	time: string;
+	price: number;
+}
 //place - concert *cant use event = reserved in JS
 class concert extends place {
 
