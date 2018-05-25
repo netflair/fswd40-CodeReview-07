b2e27c701a100e2e088c571d77655ea939102570
Patrick Klostermann <dev@netflair.at>
2018-05-25 14:50:00 +0200

    -

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+//INTERFACES FOR CLASSES -----------------------------------------------------------------------------
+//CLASSES ---------------------------------------------------------------------------------------------
+//SUPER place *cant use location = reserved in JS
 var place = /** @class */ (function () {
     function place(image, address, zip, city) {
         this.image = image;
@@ -16,9 +19,11 @@ var place = /** @class */ (function () {
         this.city = city;
     }
     place.prototype.render = function () {
+        return "Image: " + this.image + " | Adress: " + this.address + " | ZIP: " + this.zip + " | City: " + this.city;
     };
     return place;
 }());
+//place > restaurant 
 var restaurant = /** @class */ (function (_super) {
     __extends(restaurant, _super);
     function restaurant(image, address, zip, city, tel, web, type) {
@@ -30,3 +35,25 @@ var restaurant = /** @class */ (function (_super) {
     }
     return restaurant;
 }(place));
+//place > concert *cant use event = reserved in JS
+var concert = /** @class */ (function (_super) {
+    __extends(concert, _super);
+    function concert(image, address, zip, city, date, time, price) {
+        var _this = _super.call(this, image, address, zip, city) || this;
+        _this.date = date;
+        _this.time = time;
+        _this.price = price;
+        return _this;
+    }
+    return concert;
+}(place));
+//CREATE OBJECTS IN ARRAY ----------------------------------------------------------------------------
+//Places
+var places = [
+    new place("Pic1", "Address1", "Zip1", "City1"),
+    new place("Pic2", "Address2", "Zip2", "City2")
+];
+for (var _i = 0, places_1 = places; _i < places_1.length; _i++) {
+    var value = places_1[_i];
+    console.log(value.render());
+}
