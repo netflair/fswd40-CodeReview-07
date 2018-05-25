c8ba0ebc7f250f7a4584cdfee23e172a71860f28
Patrick Klostermann <dev@netflair.at>
2018-05-25 14:49:28 +0200

    restructred file | added interfaces | added test array for places |added test for-of loop to output places in console

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
+//INTERFACES FOR CLASSES -----------------------------------------------------------------------------
+
 //place interface
 interface iPlace {
 	image: string;
 	address: string;
 	zip: number;
 	city: string;
-}
-//SUPER place *cant use location = reserved in JS
-class place implements iPlace{
-
-	constructor(public image, public address, public zip, public city ){}
-
-	render(){
-
-	}
 }
 
 //restaurant interface
@@ -21,7 +14,31 @@ interface iRestaurant {
 	web: string;
 	type: string;
 }
-//place - restaurant 
+
+//concert interface
+interface iConcert {
+	date: string;
+	time: string;
+	price: number;
+}
+
+
+
+//CLASSES ---------------------------------------------------------------------------------------------
+
+//SUPER place *cant use location = reserved in JS
+class place implements iPlace{
+
+	constructor(public image, public address, public zip, public city ){}
+
+	render(){
+		return `Image: ${this.image} | Adress: ${this.address} | ZIP: ${this.zip} | City: ${this.city}`;
+	}
+
+}
+
+
+//place > restaurant 
 class restaurant extends place implements iRestaurant {
 
 	constructor(image, address, zip, city, public tel, public web, public type){
@@ -30,16 +47,29 @@ class restaurant extends place implements iRestaurant {
 
 }
 
-//concert interface
-interface iConcert {
-	date: string;
-	time: string;
-	price: number;
-}
-//place - concert *cant use event = reserved in JS
+
+//place > concert *cant use event = reserved in JS
 class concert extends place {
 
 	constructor(image, address, zip, city, public date, public time, public price){
 		super(image, address, zip, city);
 	}
+
+}
+
+
+
+//CREATE OBJECTS IN ARRAY ----------------------------------------------------------------------------
+
+//Places
+let places = [
+new place("Pic1", "Address1", "Zip1", "City1"),
+new place("Pic2", "Address2", "Zip2", "City2")
+]
+
+
+for (var value of places){
+
+	console.log(value.render());
+
 }
