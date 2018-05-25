b11e2a258d2a28a89c3010d5822812eaa1b9f2e7
Patrick Klostermann <dev@netflair.at>
2018-05-25 16:19:11 +0200

    changed render() to return html | added render2() for testing | added array for restaurants and for-of loop

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,22 @@ class place implements iPlace{
 	constructor(public image, public address, public zip, public city ){}
 
 	render(){
-		return `Image: ${this.image} | Adress: ${this.address} | ZIP: ${this.zip} | City: ${this.city}`;
+		return `
+			<div class="col-lg-6 border">
+				<div class="row no-gutters">
+					<div class="col-lg-5 border">
+						<img src="${this.image}" alt="${this.address}-Image">
+					</div>
+					<div class="col-lg-7 border">
+						<address>
+							${this.address}
+							${this.zip}
+							${this.city}
+						</address>
+					</div>
+				</div>
+			</div>
+		`
 	}
 
 }
@@ -43,6 +58,10 @@ class restaurant extends place implements iRestaurant {
 
 	constructor(image, address, zip, city, public tel, public web, public type){
 		super(image, address, zip, city);
+	}
+
+	render2(){
+		return `${super.render()}TEL: ${this.tel} | WEB: ${this.web} | TYPE: ${this.type}`;
 	}
 
 }
@@ -63,13 +82,27 @@ class concert extends place {
 
 //Places
 let places = [
-new place("Pic1", "Address1", "Zip1", "City1"),
-new place("Pic2", "Address2", "Zip2", "City2")
+	new place("Pic1", "Address1", "Zip1", "City1"),
+	new place("Pic2", "Address2", "Zip2", "City2")
 ]
 
+//Restaurants
+let restaurants = [
+	new restaurant("rpic1", "radd1", "rzip1", "rcity1", "tel1", "web1", "tpye1"),
+	new restaurant("rpic2", "radd2", "rzip2", "rcity2", "tel2", "web2", "tpye2")
 
-for (var value of places){
+]
 
-	console.log(value.render());
+//LOOP THROUGH ARRAYS AND OUTPUT ON PAGE -------------------------------------------------------------
 
+//Places
+for (let pla of places){
+
+	$('#test').append(pla.render());
+
+}
+
+//Restaurants
+for (let res of restaurants){
+	console.log(res.render2());
 }
