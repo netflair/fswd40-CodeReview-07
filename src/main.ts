22d520b8e7c479d03e83b3242ffd1a9e14701b56
Patrick Klostermann <dev@netflair.at>
2018-05-26 16:19:18 +0200

    added render() to concert class | added newDate() in all[] | added parameters for render() | added sorting funcionality

--- src/main.ts
+++ src/main.ts
@@ -23,31 +23,29 @@ interface iConcert {
 }
 
 
-
 //CLASSES ---------------------------------------------------------------------------------------------
 
-//SUPER place *cant use location = reserved in JS
+//SUPER place 
 class place implements iPlace{
 
-	constructor(public image, public address, public zip, public city ){}
+	constructor(public posted, public title, public image, public address, public zip, public city ){}
 
-	render(){
-		return `
-			<div class="col-lg-6 border">
-				<div class="row no-gutters">
-					<div class="col-lg-5 border">
-						<img src="${this.image}" alt="${this.address}-Image">
+	render(details = `${this.address}<br>${this.zip}${this.city}<br>`){
+
+		$('#test').append(`
+			<div class="col-lg-3 col-md-6 col-12 entry" data-posted="${this.posted.getTime()}">
+				<div class="row pic no-gutters">
+					<div class="col-lg-12 text-center"><h4>${this.title}</h4></div>
+					<div class="col-lg-12 d-none d-md-block">
+						<img class="img-fluid" src="${this.image}" alt="${this.address}-Image">
 					</div>
-					<div class="col-lg-7 border">
-						<address>
-							${this.address}
-							${this.zip}
-							${this.city}
-						</address>
+					<div class="col-lg-12 details border-top border-bottom">
+						${details}
 					</div>
+					<small class="posted">Created: ${this.posted.toLocaleString("de-DE")}</small>
 				</div>
 			</div>
-		`
+		`)
 	}
 
 }
@@ -56,23 +54,46 @@ class place implements iPlace{
 //place > restaurant 
 class restaurant extends place implements iRestaurant {
 
-	constructor(image, address, zip, city, public tel, public web, public type){
-		super(image, address, zip, city);
+	constructor(posted, title, image, address, zip, city, public tel, public web, public type){
+		super(posted, title, image, address, zip, city);
 	}
 
-	render2(){
-		return `${super.render()}TEL: ${this.tel} | WEB: ${this.web} | TYPE: ${this.type}`;
+	render(details) {
+
+		details = `
+			${this.address}<br>
+			${this.zip} ${this.city}<br>
+			${this.tel}<br>
+			<a href="${this.web}" target="_blank">${this.web}</a><br>
+			<small>${this.type}</small>
+		`;
+
+		super.render(details);
+		
 	}
 
 }
 
 
-//place > concert *cant use event = reserved in JS
+//place > concert 
 class concert extends place {
 
-	constructor(image, address, zip, city, public date, public time, public price){
-		super(image, address, zip, city);
+	constructor(posted, title, image, address, zip, city, public date, public time, public price){
+		super(posted, title, image, address, zip, city);
 	}
+
+	render(details) {
+
+		details = `
+			${this.address}<br>
+			${this.zip} ${this.city}<br>
+			<strong>${this.date}${this.time}</strong><br>
+			<span class="float-right text-danger">sold out <del>${this.price}</del></span>
+		`;
+
+		super.render(details);
+		
+	}	
 
 }
 
@@ -80,29 +101,41 @@ class concert extends place {
 
 //CREATE OBJECTS IN ARRAY ----------------------------------------------------------------------------
 
-//Places
-let places = [
-	new place("Pic1", "Address1", "Zip1", "City1"),
-	new place("Pic2", "Address2", "Zip2", "City2")
+let all = [
+	new restaurant(new Date("2018-05-20T23:00"), "Soba Noodle Bar", "./img/noodlebar.jpg", "Rechte Wienzeile 37", "1040", "Vienna", "+43 1 286 57 60", "http://www.soba.wien/", "japanese"),
+	new restaurant(new Date("2017-02-03T14:10"), "Motto", "./img/motto.jpg", "Schönbrunner Straße 30", "1050", "Vienna", "+43 1 5870672", "http://www.motto.wien/", "austrian"),
+	new concert(new Date("2009-08-04T10:49"), "Metallica", "./img/metallica.jpg", "Wiener Stadthalle <br>Roland Rainer Platz 1", "1150", "Vienna", "03.06.2009", "@20:00", "63,00€"),
+	new concert(new Date("2010-09-20T13:08"), "Apocalyptica", "./img/apocalyptica.jpg", "Gasometer <br>Guglgasse 8", "1110", "Vienna", "19.10.2010", "@19:30", "78,00€"),
+	new place(new Date("2005-04-04T12:45"), "Trelleborg", "./img/viking.jpg", "Trelleborg Alle 4", "4200", "Denmark - Slagelse"),
+	new place(new Date("2007-05-12T08:12"), "Hamburg Dungeon","./img/dungeon.jpg", "Kehrwieder 2", "20457", "Germany - Hamburg")
 ]
 
-//Restaurants
-let restaurants = [
-	new restaurant("rpic1", "radd1", "rzip1", "rcity1", "tel1", "web1", "tpye1"),
-	new restaurant("rpic2", "radd2", "rzip2", "rcity2", "tel2", "web2", "tpye2")
 
-]
 
-//LOOP THROUGH ARRAYS AND OUTPUT ON PAGE -------------------------------------------------------------
 
-//Places
-for (let pla of places){
+//LOOP THROUGH ARRAY AND OUTPUT ON PAGE -------------------------------------------------------------
 
-	$('#test').append(pla.render());
-
+for (let one of all){
+	let details;
+	one.render(details);
 }
 
-//Restaurants
-for (let res of restaurants){
-	console.log(res.render2());
-}
+
+
+//SORTING-------------------------------------------------------------------------------------------
+//Ascending
+$('#asc').on('click', function() {
+    var sorted = $(".entry").sort(function(a, b) {
+        return a.getAttribute('data-posted') > b.getAttribute('data-posted');
+    });
+    //detach cards and append again to container
+    sorted.detach().hide().fadeIn(450).appendTo('#test');
+});
+//Descending
+$('#desc').on('click', function() {
+    var sorted = $(".entry").sort(function(a, b) {
+        return a.getAttribute('data-posted') < b.getAttribute('data-posted');
+    });
+    //detach cards and append again to container
+    sorted.detach().hide().fadeIn(450).appendTo('#test');
+});
